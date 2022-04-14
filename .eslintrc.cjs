3125fa2d3e2b2d91d70c8c8813cf446e07fd4994
Philip Johnston <tunderadev@gmail.com>
2022-04-14 15:01:27 -0400

    fix linting

---
+++ .eslintrc.cjs
@@ -0,0 +1,48 @@
+/**
+ * @type {import('eslint').Linter.Config}
+ */
+module.exports = {
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:import/recommended',
+    'plugin:import/typescript',
+    'plugin:prettier/recommended',
+  ],
+  plugins: ['import'],
+  rules: {
+    'import/no-anonymous-default-export': 'warn',
+  },
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    requireConfigFile: false,
+    sourceType: 'module',
+    allowImportExportEverywhere: true,
+    warnOnUnsupportedTypeScriptVersion: true,
+  },
+  env: {
+    browser: true,
+    node: true,
+  },
+  settings: {
+    'import/parsers': {
+      [require.resolve('@typescript-eslint/parser')]: ['.ts', '.d.ts'],
+    },
+    'import/resolver': {
+      [require.resolve('eslint-import-resolver-node')]: {
+        extensions: ['.js', '.ts'],
+      },
+      [require.resolve('eslint-import-resolver-typescript')]: {
+        alwaysTryTypes: true,
+      },
+    },
+  },
+  overrides: [
+    {
+      files: ['**/*.cjs'],
+      rules: {
+        '@typescript-eslint/no-var-requires': 'off',
+      },
+    },
+  ],
+}
